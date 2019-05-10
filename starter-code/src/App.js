3e23b3393617ae561cbf1c84247f31dc0ffd7b40
Rafael Torres Escobar <torresc.rafael@gmail.com>
2019-05-10 00:48:27 -0500

    init config

---
+++ starter-code/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom"
+import Navbar from "./components/Navbar"
+
+
+
+function App(){
+    return(
+        <div>
+            <h1>Hi, My First React App</h1>
+        </div>
+    )
+}
+
+export default App;
